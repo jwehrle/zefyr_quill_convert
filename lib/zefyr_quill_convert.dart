ea884be2488c7f53d51ae3ffcd0b39e13d88f631
John Wehrle <wehrlej@gmail.com>
2021-03-26 10:00:16 -0700

    Associates zefyr attribute keys and flutter_quill attribute keys of document data usually decoded from a json string.

---
+++ lib/zefyr_quill_convert.dart
@@ -0,0 +1,65 @@
+library zefyr_quill_convert;
+
+/// Converts a list of zefyr deltas (usually decoded from JSON) to a list of
+/// flutter_quill deltas.
+List<Map<String, dynamic>> toQuillData(List zefyrData) {
+  List<Map<String, dynamic>> quillData = [];
+  zefyrData.forEach((zefyrDelta) {
+    Map<String, dynamic> quillDelta = {};
+    if (zefyrDelta.containsKey('insert')) {
+      quillDelta['insert'] = zefyrDelta['insert'];
+    }
+    if (zefyrDelta.containsKey('delete')) {
+      quillDelta['delete'] = zefyrDelta['delete'];
+    }
+    if (zefyrDelta.containsKey('retain')) {
+      quillDelta['retain'] = zefyrDelta['retain'];
+    }
+    if (zefyrDelta.containsKey('attributes')) {
+      quillDelta['attributes'] =
+          _toQuillAttributes(zefyrDelta['attributes'] as Map);
+    }
+    quillData.add(quillDelta);
+  });
+  return quillData;
+}
+
+Map<String, dynamic> _toQuillAttributes(Map zefyrAttribute) {
+  Map<String, dynamic> quillAttr = {};
+  if (zefyrAttribute.containsKey('b')) {
+    quillAttr['bold'] = zefyrAttribute['b'];
+  }
+  if (zefyrAttribute.containsKey('i')) {
+    quillAttr['italic'] = zefyrAttribute['i'];
+  }
+  if (zefyrAttribute.containsKey('heading')) {
+    quillAttr['header'] = zefyrAttribute['heading'];
+  }
+  if (zefyrAttribute.containsKey('s')) {
+    quillAttr['strike'] = zefyrAttribute['s'];
+  }
+  if (zefyrAttribute.containsKey('a')) {
+    quillAttr['link'] = zefyrAttribute['a'];
+  }
+  if (zefyrAttribute.containsKey('block')) {
+    if (zefyrAttribute['block'] == 'ul') {
+      quillAttr['list'] = 'bullet';
+    }
+    if (zefyrAttribute['block'] == 'ol') {
+      quillAttr['list'] = 'ordered';
+    }
+    if (zefyrAttribute['block'] == 'quote') {
+      quillAttr['blockquote'] = true;
+    }
+    if (zefyrAttribute['block'] == 'code') {
+      quillAttr['code-block'] = true;
+    }
+  }
+  if (zefyrAttribute.containsKey('embed')) {
+    Map embed = zefyrAttribute['embed'] as Map;
+    if (embed['type'] == 'image') {
+      quillAttr['image'] = embed['source'];
+    }
+  }
+  return quillAttr;
+}
